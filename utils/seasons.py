b9e5c066040cb954b5bf1f72be284b0a26dc00ea
Felicia123-prog <barchafel@gmail.com>
2026-05-14 11:53:31 -0300

    Add function to assign seasons based on month

--- utils/seasons.py
+++ utils/seasons.py
@@ -1,1 +1,11 @@
+def assign_season(row):
+    m = row["month"]
+    if m in [12,1,2]:
+        return "Kleine regentijd"
+    if m in [3,4]:
+        return "Kleine droge tijd"
+    if m in [5,6,7,8]:
+        return "Grote regentijd"
+    if m in [9,10,11]:
+        return "Grote droge tijd"
 
