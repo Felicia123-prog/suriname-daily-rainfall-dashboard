d6478e81dfad1e9f44a0731fc2634eda7719af07
Felicia123-prog <barchafel@gmail.com>
2026-05-14 11:52:53 -0300

    Add function to load and process CSV data

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def load_rr(year):
+    df = pd.read_csv(f"data/rr_{year}.csv")
+    df['date'] = pd.to_datetime(df['date'])
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
+    df['day'] = df['date'].dt.day
+    return df
 
